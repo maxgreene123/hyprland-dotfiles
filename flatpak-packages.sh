bb95076bc17bc4346aa9e4390855a7c27cbc4d52
Max Greene <101725496+maxgreene123@users.noreply.github.com>
2024-10-20 23:04:51 -0400

    Update flatpak-packages.sh

--- flatpak-packages.sh
+++ flatpak-packages.sh
@@ -4,13 +4,10 @@
 echo "Adding Flathub repository if not already added..."
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-# Install SyncThingy package
-echo "Installing SyncThingy..."
-flatpak install -y flathub com.github.zocker_160.SyncThingy
-
-# Install Flatseal package
-echo "Installing Flatseal..."
 flatpak install -y flathub com.github.tchx84.Flatseal
 
+flatpak install -y flathub io.github.martchus.syncthingtray
+
+flatpak install -y flathub com.usebottles.bottles
 # Display success message
 echo "flatpak packages installed successfully!"
